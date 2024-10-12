853c3b370ef4c42952615c03427179b39dfc0bf3
Emma <ainsgreyrat@gmail.com>
2024-10-11 23:41:01 -0500

    3.10

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors())
 morgan.token('body', function gePostToken(req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('dist'))
+
 let persons = [
   {
     "id": 1,
